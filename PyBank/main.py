007985128a572c1ab204798e749fe9afe3ba878b
CriscentWolf <criscentwolf@gmail.com>
2023-04-12 05:28:58 -0400

    PyBank working build

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ greatest_increase_month = ""
 greatest_decrease = 0
 greatest_decrease_month = ""
 
-with open('Resources/budget_data.csv') as csvfile:
+with open('Resources/budget_data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     for row in csvreader:
@@ -27,22 +27,28 @@ with open('Resources/budget_data.csv') as csvfile:
         if profit_change != profit:
             profit_list.append(profit_change)
         
-        if greatest_increase < profit_change:
+        if profit_change > greatest_increase:
             greatest_increase = profit_change
             greatest_increase_month = month
-        if greatest_decrease > profit_change:
+        if profit_change < greatest_decrease:
             greatest_decrease = profit_change
             greatest_decrease_month = month
         
     profit_average = "{0:.2f}".format(sum(profit_list)/len(profit_list))
 
-
-    #print(profit_average)
-
-    print(f'''    Financial Analysis
+with open('analysis/analysis_output.txt', 'w') as file:
+    file.write(f'''    Financial Analysis
 ----------------------------
 Total Months: {total_months}
 Total: ${total_profit}
 Average Change: ${profit_average}
 Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})
-Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})   ''')
+Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})''')
+
+print(f'''    Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${total_profit}
+Average Change: ${profit_average}
+Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})
+Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})''')
