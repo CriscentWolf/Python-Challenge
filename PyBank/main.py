3a57916a03acf2114e3a4f7cf76129e5cfcfca00
CriscentWolf <criscentwolf@gmail.com>
2023-04-12 06:09:53 -0400

    PyBank Header stored and comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import csv
 
+# Set Initial Variables
 total_months = 0
 total_profit = 0
 profit_change = 0
@@ -13,12 +14,16 @@ greatest_decrease_month = ""
 
 with open('Resources/budget_data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
+    
+    # Header Stored
+    header = next(csvreader)
     for row in csvreader:
 
+        # Variables to clean up calculations
         month = str(row[0])
         profit = int(row[1])
 
+        # Sums for months and profits
         total_months += 1
         total_profit += profit
 
@@ -27,6 +32,7 @@ with open('Resources/budget_data.csv', 'r') as csvfile:
         if profit_change != profit:
             profit_list.append(profit_change)
         
+        # Comparisons to find Greatest increase and decrease
         if profit_change > greatest_increase:
             greatest_increase = profit_change
             greatest_increase_month = month
@@ -34,8 +40,10 @@ with open('Resources/budget_data.csv', 'r') as csvfile:
             greatest_decrease = profit_change
             greatest_decrease_month = month
         
+    # Formatting and Calculations to find the average change in profits from month to month
     profit_average = "{0:.2f}".format(sum(profit_list)/len(profit_list))
 
+#Outputs
 with open('analysis/analysis_output.txt', 'w') as file:
     file.write(f'''    Financial Analysis
 ----------------------------
